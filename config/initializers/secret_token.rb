8c70795d39e0761f1ab6083c1d92c89a35880baf
Espen Antonsen <espen@inspired.no>
2011-05-14 16:55:07 +0800

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kowoko::Application.config.secret_token = '97b2ed6a1ab67e8e143ddb3b8c6eddca8de28dcc0585dcbe328e535618e442707830bf4d7620340c2ed509f53ad4e089dc65265cecc03af9740ff735ee7b2ecf'
