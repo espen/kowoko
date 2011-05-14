982ddc939dd360d376892dbb89ddbf61f6038742
Espen Antonsen <espen@inspired.no>
2011-05-14 17:40:39 +0800

    auth settings load

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load app vars from local file
+kowoko_env = File.join(Rails.root, 'config', 'kowoko.rb')
+load(kowoko_env) if File.exists?(kowoko_env)
+
 # Initialize the rails application
 Kowoko::Application.initialize!
