0f8367b29432bbc7906fe8d6c3be3a82bb926f3f
Espen Antonsen <espen@inspired.no>
2011-05-14 18:00:18 +0800

    added events route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Kowoko::Application.routes.draw do
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+  
+  resources :events
+  
+  root :to => "events#index"
+  
 end
