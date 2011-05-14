83b83d3587895487062ede430f5c8df522f15ea6
Espen Antonsen <espen@inspired.no>
2011-05-14 23:27:54 +0800

    login required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
+  def login_required
+    redirect_to "/auth/twitter" if !current_user
+  end
 
 end
